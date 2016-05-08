b0ce5d5a98f28452ce0adc93f2eb1372c18ab1a8
nkondety <nkondety@gmail.com>
2016-05-08 15:42:34 -0500

    Initial writing

---
+++ SpringDemo/src/org/naren/soap/DrawingApp.java
@@ -0,0 +1,10 @@
+package org.naren.soap;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SpringDemo/src/org/naren/soap/Triangle.java
@@ -0,0 +1,8 @@
+package org.naren.soap;
+
+public class Triangle {
+	public void area(){
+		System.out.println("triangle area");
+	}
+
+}
